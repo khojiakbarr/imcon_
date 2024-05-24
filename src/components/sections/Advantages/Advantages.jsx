dbe47a1709d8f6d7c0d532df4247898dba520609
khojiakbarr <chinozbekov09@gmail.com>
2024-05-24 19:48:50 +0500

    up

--- src/components/sections/Advantages/Advantages.jsx
+++ src/components/sections/Advantages/Advantages.jsx
@@ -43,6 +43,9 @@ export default function Advantages() {
                   solutions that align perfectly with your unique business goals
                   and needs.
                 </p>
+
+
+                
               </div>
             </div>
             <div className="flex justify-center mt-[30px]">
